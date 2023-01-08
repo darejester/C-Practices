4bf6dfd1e50b46f1ebb8dbcfeff627d0e3125d20
John S <53415886+darejester@users.noreply.github.com>
2023-01-08 18:11:42 -0500

    Update main.cpp

--- LinkedListTwoSum/main.cpp
+++ LinkedListTwoSum/main.cpp
@@ -3,18 +3,20 @@
 #include <vector>
 #include <string>
 
+using namespace std;
+
 struct ListNode {
     //holds value
     int val;
     //pointer to ListNode object
-    ListNode *next;
+    ListNode* next;
     //default constructor
     ListNode() : val(0), next(nullptr) {}
     //constructor if there is a value given
     ListNode(int x) : val(x), next(nullptr) {}
     //constructor if there is a value and another ListNode object is given
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-    
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+
 };
 
 int main()
@@ -22,65 +24,58 @@ int main()
     //input l1
     ListNode temp1(3);
     ListNode temp2(4, &temp1);
-    ListNode l1(2, &temp2);
+    ListNode input1(2, &temp2);
+    ListNode* l1 = &input1;
     //inputl2
     ListNode temp3(4);
     ListNode temp4(6, &temp3);
-    ListNode l2(5,&temp4);
+    ListNode input2(5, &temp4);
+    ListNode* l2 = &input2;
 
-    //container for final answer
-    ListNode answer;
-    //pointers towards nodes
-    ListNode* ptr1 = &l1;
-    ListNode* ptr2 = &l2;
-    ListNode* ptr3 = &answer;
-    ListNode* ptr4 = &answer;
-    //holds value for any carry-overs
+    //pointer to answer ListNode
+    ListNode* answer = new ListNode();
+    //pointer to iterate through answer
+    ListNode* temp = answer;
+    //container for carry-overs
     int carry = 0;
-    //hold sum of vals
+    //container for sum
     int sum = 0;
-
-    //go through all linked list
-    while (ptr1 != nullptr || ptr2 != nullptr)
-    {
-        //add nodes and any carry-overs
-        sum = ptr1->val + ptr2->val + carry;
-        //check if there is any carry-overs for next node
-        if (sum > 9)
-        {
-            carry = 1;
-            sum = sum - 10;
+    //loop through whole of l1 and l2 and if there is a carry
+    while (l1 != NULL || l2 != NULL || carry) {
+        if (l1 != NULL) {
+            sum += l1->val;
+            l1 = l1->next;
         }
-        //set sum as the value of the node in answer
-        ptr3->val = sum;
-        //check if there are any other nodes to be added
-        if (ptr1->next != nullptr || ptr2->next != nullptr)
-        {
-            //add new node to answer
-            ptr3->next = new ListNode;
-            ptr3 = ptr3->next;
+        if (l2 != NULL) {
+            sum += l2->val;
+            l2 = l2->next;
         }
-        //check if there are more nodes left in any list
-        if (ptr1 != nullptr)
+        //add carry to current sum
+        sum += carry;
+        //set new carry
+        carry = sum / 10;
+        //set current sum
+        sum = sum % 10;
+        temp->val = sum;
+        //equal sum to 0 for next node
+        sum = 0;
+        //create next answer node if either l1 or l2 still has objects
+        if (l1 != nullptr || l2 != nullptr || carry)
         {
-            //move pointers to next node
-            ptr1 = ptr1->next;
-            
+            temp->next = new ListNode;
+            temp = temp->next;
         }
-        if (ptr2 != nullptr)
-        {
-            ptr2 = ptr2->next;
-        }
-        
-
 
     }
-    while (ptr4 != nullptr)
+
+    while (answer != nullptr)
     {
-        std::cout << ptr4->val;
-        ptr4 = ptr4->next;
+        std::cout << answer->val;
+        answer = answer->next;
     }
 
-    //std::cout << answer.next << std::endl;
+
+
+    return 0;
 
 }
